929d189a07e768ef3e9d616dd5dfb8971af81dab
Thomas Waters <Thomas.waters05@gmail.com>
2017-03-06 18:01:25 -0800

    created reverse models

---
+++ FakeTrello/ReverseModels/AspNetUser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.ReverseModels
+{
+    public class AspNetUser
+    {
+        [Key]
+        [MaxLength(128)]
+        public string Id { get; set; }
+
+        public string Email { get; set; }
+
+        [Required]
+        public bool EmailConfirmed { get; set; }
+
+        public string PasswordHash { get; set; }
+        public string SecurityStamp { get; set; }
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string PhoneNumberConfirmed { get; set; }
+
+        [Required]
+        public bool TwoFactorEnabled { get; set; }
+
+        public DateTime LockoutEndDateUTC { get; set;}
+
+        [Required]
+        public bool LockoutEnabled { get; set; }
+
+        [Required]
+        public int AccessFailedCount { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public string UserName { get; set; }
+
+
+    }
+}
