5dbba82da888c8c84730f7cdf710373529088874
Thomas Waters <Thomas.waters05@gmail.com>
2017-03-11 12:01:24 -0800

    tweaked code to consolidate, added collab class

---
+++ FakeTrello/DAL/Irepository.cs
@@ -0,0 +1,41 @@
+﻿using FakeTrello.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeTrello.DAL
+{
+    interface Irepository
+    {
+        //List of methods to help deliver features
+        void AddBoard(string name, TrelloUser owner);
+        void AddList(string name, Board board);
+        void AddList(string name, int boardId);
+        void AddCard(string name, List list, TrelloUser owner);
+        void AddCard(string name, int ListId, int ownerId);
+
+        //Read
+        List<Card> GetCardsFromList(int listId);
+        List<Card> GetCardsFromBoard(int boardId);
+        Card GetCard(int cardId);
+        List GetList(int listId);
+        List<List> GetListsFromBoard(int boardId); // List of Trello Lists
+        List<Board> GetBoardsFromUser(int userId);
+        Board GetBoard(int boardId);
+        List<TrelloUser> GetCardAttendees(int cardId);
+
+        // Update
+        bool AttachUser(int userId, int CardId); // true successful, false: not successful
+            //Move
+        bool MoveCard(int cardId, int oldListId, int newListId);
+            //Copy
+        bool CopyCard(int CardId, int newListId, int newOwnerId);
+
+        //Delete
+        bool RemoveBoard(int BoardId);
+        bool RemoveList(int ListId);
+        bool RemoveCard(int cardId);
+    }
+}
---
+++ FakeTrello/Models/Collaborator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.Models
+{
+    public class Collaborator
+    {
+        [Key]
+        public int CollaboratorId { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        public int CardId { get; set; }
+    }
+}
