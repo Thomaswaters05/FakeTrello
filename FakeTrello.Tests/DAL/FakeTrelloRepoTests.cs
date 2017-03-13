e41add26c1db5f9af2d0de07a630adb10f3dc4f9
Thomas Waters <Thomas.waters05@gmail.com>
2017-03-13 16:56:20 -0700

    added FakeTrelloRepTest file + nec changes for this file

---
+++ FakeTrello.Tests/DAL/FakeTrelloRepoTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FakeTrello.DAL;
+
+namespace FakeTrello.Tests.DAL
+{
+    [TestClass]
+    public class FakeTrelloRepoTests
+    {
+        [TestMethod]
+        public void EnsureICanCreateInstanceofRepo()
+        {
+            FakeTrelloRepository repo = new FakeTrelloRepository();
+
+            Assert.IsNotNull(repo);
+        }
+
+        [TestMethod]
+        public void EnsureICanInjectContextInstance()
+        {
+            FakeTrelloContext context = new FakeTrelloContext();
+            FakeTrelloRepository repo = new FakeTrelloRepository(context);
+
+            Assert.IsNotNull(repo.Context);
+        }
+
+        [TestMethod]
+        public void EnsureIHaveNotNullContext()
+        {
+            FakeTrelloContext context = new FakeTrelloContext();
+            FakeTrelloRepository repo = new FakeTrelloRepository(context);
+
+            Assert.IsNotNull(repo.Context);
+        }
+    }
+}
